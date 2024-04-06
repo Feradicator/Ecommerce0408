cb162a10f729ecf8867087ff26712b9641077d24
Feradicator <yadavshivam042001@gmail.com>
2024-04-06 13:50:32 +0530

    added navigation and main carosel component

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       hello there
+      <Navigation/>
     </div>
   );
 }
