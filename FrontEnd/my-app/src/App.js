dd3c6e57fd579b2acc71116233de1fa37c2fa5c4
Feradicator <yadavshivam042001@gmail.com>
2024-04-08 18:15:49 +0530

    added produt filters from tailwind css

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
+import Product from './customer/components/Product/Product';
 
 function App() {
   return (
     <div className="App">
       
       <Navigation/>
-      <div>
+      {/* <div>
       <HomePage/>
+      </div> */}
+      <div>
+        <Product/>
       </div>
       <Footer/>
     </div>
