f9b83c8750a29bcd1d0806b2597cd16ad14a3bb9
Feradicator <yadavshivam042001@gmail.com>
2024-04-09 17:42:30 +0530

    product page added

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './customer/components/navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 
 function App() {
   return (
@@ -13,8 +14,11 @@ function App() {
       {/* <div>
       <HomePage/>
       </div> */}
-      <div>
+      {/* <div>
         <Product/>
+      </div> */}
+      <div>
+        <ProductDetails/>
       </div>
       <Footer/>
     </div>
--- FrontEnd/my-app/src/customer/components/Product/ProductCard.jsx
+++ FrontEnd/my-app/src/customer/components/Product/ProductCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import "./ProductCard.css"
-const ProductCard = () => {
+const ProductCard = ({product}) => {
     return (
         <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
             <div className='h-[20rem]'>
-                <img className='h-full w-full object-cover object-left-top' src="https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/s/v/3/l-nvk-4444-nivick-original-imags6frqegpxe7q.jpeg?q=70" alt="" />
+                <img className='h-full w-full object-cover object-left-top' src={product.imageUrl} alt="" />
             </div>
             <div className='textPart bg-white p-3'>
                 <div>
-                    <p className='font-bold opacity-60'>Universaloutfit</p>
-                    <p>Casual Puff Sleeves Solid Women White Top</p>
+                    <p className='font-bold opacity-60'>{product.brand}</p>
+                    <p>{product.title}</p>
                 </div>
                 <div className='flex items-center space-x-2'>
-                    <p className='font-semibold' >*199</p>
-                    <p className='line-through opacity-50'>*1999</p>
-                    <p className='text-green-600 font-semibold' >70% off</p>
+                    <p className='font-semibold' >{product.discountedPrice}</p>
+                    <p className='line-through opacity-50'>{product.price}</p>
+                    <p className='text-green-600 font-semibold' >{product.discountPersent}% off</p>
                 </div>
             </div>
         </div>
