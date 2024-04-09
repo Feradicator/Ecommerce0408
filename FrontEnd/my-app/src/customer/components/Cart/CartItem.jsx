cccb64d1a1f28c220551d63963d2fc82ddef2f37
Feradicator <yadavshivam042001@gmail.com>
2024-04-10 03:37:54 +0530

    added cart page

--- FrontEnd/my-app/src/customer/components/Cart/CartItem.jsx
+++ FrontEnd/my-app/src/customer/components/Cart/CartItem.jsx
@@ -4,7 +4,7 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 const CartItem = () => {
     return (
-        <div className='p-5 shadow-1g border rounded-md'>
+        <div className='p-5 shadow-lg border rounded-md'>
             <div className='flex items-center'>
                 <div className='w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]'>
                     <img className='w-full h-full object-cover object-top' src="https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70"
@@ -31,7 +31,7 @@ const CartItem = () => {
                         <RemoveCircleOutlineIcon />
                     </IconButton>
                     <span className='py-1 px-7 border rounded-sm'>3</span> 
-                    <IconButton sx={{ color: "RGB (145 85 253)" }}>
+                    <IconButton sx={{ color: "RGB(145 85 253)" }}>
                         <AddCircleOutlineIcon />
                     </IconButton>
                 </div>
