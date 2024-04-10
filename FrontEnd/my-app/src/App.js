10052d78f8552b4fb3a8e37d8f6f19360f1b6136
Feradicator <yadavshivam042001@gmail.com>
2024-04-10 15:14:46 +0530

    checkout page and address card ready

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -6,6 +6,7 @@ import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
 
 function App() {
   return (
@@ -21,8 +22,11 @@ function App() {
       {/* <div>
         <ProductDetails/>
       </div> */}
-      <div>
+      {/* <div>
         <Cart/>
+      </div> */}
+      <div>
+        <Checkout/>
       </div>
       <Footer/>
     </div>
