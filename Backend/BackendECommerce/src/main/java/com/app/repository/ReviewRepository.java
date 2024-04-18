1a48901888e6e206bc9eee22c4078137cce4b6cc
Feradicator <yadavshivam042001@gmail.com>
2024-04-19 04:36:45 +0530

    added rating repo and review repo

---
+++ Backend/BackendECommerce/src/main/java/com/app/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.app.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.model.Review;
+public interface ReviewRepository extends JpaRepository<Review,Long> {
+
+}
