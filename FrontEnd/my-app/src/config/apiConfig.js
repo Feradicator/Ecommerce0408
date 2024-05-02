9a18b1e29612f83563995e03db6e83076342c7e3
Feradicator <yadavshivam042001@gmail.com>
2024-05-02 23:24:21 +0530

    added server config

--- FrontEnd/my-app/src/config/apiConfig.js
+++ FrontEnd/my-app/src/config/apiConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-export const API_BASE_URL="http://localhost:8080"
+export const API_BASE_URL="ecommerce0408-production.up.railway.app"
 const jwt=localStorage.getItem("jwt")
 export const api=axios.create(
     {
