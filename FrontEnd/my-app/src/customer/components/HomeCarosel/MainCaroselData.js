eebed6121c075898a683e59644217dd3e139e598
Shivam Yadav <81615808+Feradicator@users.noreply.github.com>
2025-08-21 00:40:56 +0530

    Update MainCaroselData.js

--- FrontEnd/my-app/src/customer/components/HomeCarosel/MainCaroselData.js
+++ FrontEnd/my-app/src/customer/components/HomeCarosel/MainCaroselData.js
@@ -7,10 +7,10 @@ export const MainCaroselData=[
         image:"https://www.ethnicplus.in/cdn/shop/files/8.jpg?v=1755414645&width=1880"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/7/_/7_10.jpg"
+        image:"https://www.ethnicplus.in/cdn/shop/files/poster_17-08_780x.jpg?v=1755416459"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_13.jpg"
+        image:"https://www.ethnicplus.in/cdn/shop/files/5_9ffc34cd-8045-490e-b723-5f01636efa10.jpg?v=1755190441&width=1880"
     },
     {
         image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
