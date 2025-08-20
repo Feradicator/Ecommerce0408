9801b87ffdf6b966aa883214bf650e850c54f83c
Shivam Yadav <81615808+Feradicator@users.noreply.github.com>
2025-08-21 00:43:07 +0530

    Update MainCaroselData.js

--- FrontEnd/my-app/src/customer/components/HomeCarosel/MainCaroselData.js
+++ FrontEnd/my-app/src/customer/components/HomeCarosel/MainCaroselData.js
@@ -17,7 +17,7 @@ export const MainCaroselData=[
         path:"/women/clothing/lengha_choli"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/salwar_kameez_coll_copy_1920x.jpg?v=1753980614",
         path:"/women/clothing/women_dress"
     },
     {
