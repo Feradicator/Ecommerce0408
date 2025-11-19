e1fd49ad9a1f8a6d7372432f00d3296510b244c8
Feradicator <yadavshivam042001@gmail.com>
2025-11-19 14:07:38 +0530

    added fix

--- Backend/BackendECommerce/src/main/java/com/app/config/AppConfig.java
+++ Backend/BackendECommerce/src/main/java/com/app/config/AppConfig.java
@@ -22,7 +22,7 @@ public class AppConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
 	{
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and
-		().authorizeHttpRequests(Authorize->Authorize.antMatchers("/api/** ").authenticated().anyRequest().permitAll())
+		().authorizeHttpRequests(Authorize->Authorize.antMatchers("/api/**").authenticated().anyRequest().permitAll())
 		.addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class).csrf().disable()
 		.cors().configurationSource(new CorsConfigurationSource() {
 			
