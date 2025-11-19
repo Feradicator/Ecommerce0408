8769c3cfa29ead0f4adbc6beb37a57bbd3f6f796
Feradicator <yadavshivam042001@gmail.com>
2025-11-19 12:25:09 +0530

    added fix

--- Backend/BackendECommerce/src/main/java/com/app/config/RedisConfig.java
+++ Backend/BackendECommerce/src/main/java/com/app/config/RedisConfig.java
@@ -4,6 +4,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
 import java.time.Duration;
 
 @Configuration
@@ -12,9 +15,16 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
+
         return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))   // cache expiry
+                // Store values in JSON, not Java serialization
+                .serializeValuesWith(
+                    RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                )
+                // Set TTL
+                .entryTtl(Duration.ofMinutes(10))
                 .disableCachingNullValues();
     }
 }
-
